70489dd2f845b051e1500102b560c6fc372c799d
issmarthealth <161579150+issmarthealth@users.noreply.github.com>
2024-03-02 13:53:17 +0530

    final commit

---
+++ shoolini.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- ======= topbar ======= -->
+  <?php include 'topbar.php';?> 
+  <!-- End topbar -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+		
+		<?php include 'header.php';?> 
+  
+  <!-- End Header -->
+
+  <main id="main">
+
+    <section class="single-post-content">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9 post-content" data-aos="fade-up">
+
+            <!-- ======= Single Post Content ======= -->
+            <div class="single-post">
+              About Shoolini University
+              <h1 class="mb-2">Shoolini University of Biotechnology and Management Sciences</h1>
+              <p>
+			  Founded by leading academicians and professionals, Shoolini University is a young, research-focussed, philanthropic university. Established in 2009, this dynamic and innovation-led centre of learning has made giant strides. It has consistently been ranked among India’s top 100 universities (NIRF), is one of India’s highest generators of patents and innovation, and matches the world’s best universities in research output. 
+
+ <BR><BR>
+
+A not-for-profit multi-disciplinary private university, Shoolini has a unique research and innovation-led model. We believe that a great university drives cutting-edge knowledge and innovation while working for the social and economic upliftment of the community at the same time. It is this belief – along with our internationally qualified faculty, our focus on careers and placements for students, our continuous innovation in pedagogy and our highly motivated students – that makes Shoolini University a progressive and dynamic university in the global knowledge economy.
+
+ <BR><BR>
+
+We pride ourselves in laying emphasis on the development of the creative potential of every student, providing them high-quality educational opportunities, which in turn will determine the future of our country and take India to greatness.
+
+
+			  </p>
+
+              <figure class="my-4">
+                <img src="assets/img/shoolini2.jpg" alt="" class="img-fluid" width="900px" height="800px">
+                <figcaption>Shoolini University Campus </figcaption>
+              </figure>
+              
+			  
+            </div><!-- End Single Post Content -->
+
+          </div>
+          <div class="col-md-3">
+            <!-- ======= Sidebar ======= start-->
+            <?php include 'sidebar.php';?> 
+			<!-- ======= Sidebar ======= end -->
+			
+
+           
+
+          </div>
+        </div>
+      </div>
+    </section>
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= start-->
+			  <?php include 'footer.php';?> 
+		<!-- ======= Footer ======= end-->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
